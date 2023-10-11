abf3bfa38bb114904d5c76462cc5b2fe050197bb
ram verma <ramvrm5@gmail.com>
2023-10-11 16:30:30 +0530

    changes in checkoutpage

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,13 +1,8 @@
 import React from 'react';
-
 import Image from 'next/image';
-
 import { StarIcon } from '@heroicons/react/solid';
-
 import Currency from "react-currency-formatter";
-
 import { useDispatch } from 'react-redux';
-
 import { addToBasket, removeFromBasket } from "../slices/basketSlice";
 
 const CheckoutProduct = ({ id, title, price, rating, description, category, image, hasPrime }) => {
